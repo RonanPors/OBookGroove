c2e0289c6f7ccf5d4b4876ef094d4b75f375f67d
Ronan Pors <pors.ronan@gmail.com>
2024-07-01 12:59:32 +0200

    Creating the validation schema for user registration in a database

---
+++ app/middlewares/validation.middleware.js
@@ -0,0 +1,10 @@
+// Fonction de validation des schemas qui sera utilisé par le router.
+
+export default (schema, requestProperty) => async (req, _, next) => {
+  try {
+    await schema.validateAsync(req[requestProperty]);
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ app/schemas/Joi/user.create.schema.js
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+export default Joi.object({
+  pseudo: Joi.string().min(2).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    // Définir une longueur minimale de 8 caractères
+    .min(8)
+    // Doit contenir au moins une majuscule
+    .pattern(new RegExp('(?=.*[A-Z])'))
+    // Doit contenir au moins une minuscule
+    .pattern(new RegExp('(?=.*[a-z])'))
+    // Doit contenir au moins un chiffre
+    .pattern(new RegExp('(?=.*[0-9])'))
+    // Doit contenir au moins un caractère spécial
+    .pattern(new RegExp('(?=.*[!@#$%^&*])'))
+    .required()
+    .messages({
+      'password.pattern': 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.',
+      'password.min': 'Le mot de passe doit contenir au moins 8 caractères.'}),
+  confirmPassword: Joi.any().valid(Joi.ref('password')).required().message({'any.only': 'Les mots de passe ne correspondent pas'}),
+}).required().message({'missing.data':'Aucune donnée n\'a été renseignée'});
