0b92c43ef8ec28dc50ce34345f3c0a9b6e8449f8
KuroiNekOo <maximegomes.dinis@outlook.fr>
2024-07-02 10:29:22 +0200

    refactor: :recycle: delete file test.js
    Suppression du fichier test.js qui était inutile

--- test.js
+++
@@ -1,6 +0,0 @@
-const claimsTokenVerify = (claim1, claim2) => JSON.stringify(claim1) === JSON.stringify(claim2);
-
-const obj1 = { a: 1, b: { c: 3 } };
-const obj2 = { a: 1, b: { c: 3 } };
-
-console.log(claimsTokenVerify(obj1, obj2));
