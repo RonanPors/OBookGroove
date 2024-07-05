bf8f3d58fde8025047dc7c019591cb446f6cd256
KuroiNekOo <maximegomes.dinis@outlook.fr>
2024-07-05 20:30:36 +0200

    refactor: :recycle: change env variables for PG
    Changement des variables d'env pour postgresql

--- app/db/db.js
+++ app/db/db.js
@@ -4,11 +4,11 @@ import knex from 'knex';
 export const connection = knex({
   client: 'pg',
   connection: {
-    host: process.env.PGHOST || 'localhost',
-    port: process.env.PGPORT || 5432,
-    user: process.env.PGUSER || 'spedata',
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE || 'obookgroove',
+    host: process.env.PG_HOST || 'localhost',
+    port: process.env.PG_PORT || 5432,
+    user: process.env.PG_USER || 'spedata',
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE || 'obookgroove',
   },
   //* Pool par défaut sur PG { min: 2, max: 10 }
   pool: {
