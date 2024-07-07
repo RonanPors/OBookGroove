0a40a4bac3431472a9182edd45beb79ba6789610
KuroiNekOo <maximegomes.dinis@outlook.fr>
2024-07-07 19:30:44 +0200

    feat: :sparkles: change snake_case in camelCase and camelCase in snake_case
    Changement du nommage des variables entrantes et sortantes de la BDD depuis les datamappers

--- app/datamappers/user-has-book.datamapper.js
+++ app/datamappers/user-has-book.datamapper.js
@@ -1,4 +1,5 @@
 import CoreDatamapper from './core.datamapper.js';
+import * as changeKeys from 'change-case/keys';
 
 class UserHasBook extends CoreDatamapper {
   tableName = 'user_has_book';
@@ -6,7 +7,10 @@ class UserHasBook extends CoreDatamapper {
   async findByUser(userId) {
 
     const rows = await this.client.from(this.tableName).where({ 'user_id': userId });
-    return rows;
+
+    const newRows = rows.map((row) => changeKeys.camelCase(row));
+
+    return newRows;
 
   }
 }
