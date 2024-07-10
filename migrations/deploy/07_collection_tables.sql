dc08347828ad9a48a13ce5c5a09673f931e1fa0d
KuroiNekOo <maximegomes.dinis@outlook.fr>
2024-07-10 21:34:52 +0200

    feat: :sparkles: add collection tables in bdd with sqitch migration
    Ajout des deux tables collection et collection_has_book avec une migration sqitch

---
+++ migrations/deploy/07_collection_tables.sql
@@ -0,0 +1,26 @@
+-- Deploy obookgroove:07_collection_tables to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+CREATE TABLE "collection" (
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "user_id" INT NOT NULL REFERENCES "user"("id") ON DELETE CASCADE,
+    "collection_name" TEXT,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "collection_has_book" (
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "collection_id" INT NOT NULL REFERENCES "collection"("id") ON DELETE CASCADE,
+    "book_id" INT NOT NULL REFERENCES "book"("id") ON DELETE CASCADE,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ
+);
+
+ALTER TABLE "collection_has_book"
+ADD CONSTRAINT collection_has_book_unique UNIQUE ("collection_id", "book_id");
+
+COMMIT;
