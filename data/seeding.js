351d99015317ad4fd5309c9ce677f5ae4cf27691
KuroiNekOo <maximegomes.dinis@outlook.fr>
2024-07-15 10:08:07 +0200

    feat: :sparkles: add seeding for is_read
    Ajout du seeding pour is_read

--- data/seeding.js
+++ data/seeding.js
@@ -181,38 +181,39 @@ async function insertUserHasBook() {
     (
       "book_id",
       "user_id",
+      "is_read",
       "note"
     )
     VALUES
     (
-      1, 1, 1
+      1, 1, true, 1
     ),
     (
-      2, 1, 1
+      2, 1, true, 1
     ),
     (
-      3, 1, 1
+      3, 1, true, 1
     ),
     (
-      4, 1, 1
+      4, 1, true, 1
     ),
     (
-      5, 1, 1
+      5, 1, false, 1
     ),
     (
-      6, 1, 1
+      6, 1, false, 1
     ),
     (
-      7, 1, 1
+      7, 1, false, 1
     ),
     (
-      8, 1, 1
+      8, 1, false, 1
     ),
     (
-      9, 1, 1
+      9, 1, false, 1
     ),
     (
-      10, 1, 1
+      10, 1, false, 1
     )
     RETURNING id
   `;
